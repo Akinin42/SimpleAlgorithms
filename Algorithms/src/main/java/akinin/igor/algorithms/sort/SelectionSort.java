d700d67a7c782e7d75a88fb3db31a0646e0f9be3
Igor Akinin <akinin32167@gmail.com>
2021-03-23 22:32:37 +0700

    add selection sort

---
+++ Algorithms/src/main/java/akinin/igor/algorithms/sort/SelectionSort.java
@@ -0,0 +1,31 @@
+package akinin.igor.algorithms.sort;
+
+import akinin.igor.algorithms.ArraysCreater;
+
+public class SelectionSort extends Sorting {
+
+    @Override
+    int[] sort(int arraySize) {
+        if (arraySize < 0) {
+            throw new IllegalArgumentException("You can't input negative numbers!");
+        }
+        int[] sortableArray = ArraysCreater.createArray(arraySize);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < arraySize; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arraySize; j++) {
+                if (sortableArray[j] < sortableArray[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = sortableArray[i];
+                sortableArray[i] = sortableArray[minIndex];
+                sortableArray[minIndex] = temp;
+            }
+        }
+        long finishTime = System.currentTimeMillis();
+        System.out.println(String.format("On sorting spent %s milliseconds!", (finishTime - startTime)));
+        return sortableArray;
+    }
+}
